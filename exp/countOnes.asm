5c343e07b05d8c03b8c3c9a4a5e7451b75677ebd
mjdargen <mjdargen@gmail.com>
2021-06-14 16:07:51 -0400

    updated comments for countOnes.asm

--- exp/countOnes.asm
+++ exp/countOnes.asm
@@ -10,19 +10,19 @@
 
 MYSTART GETC                ; Retrieve character entered
         OUT                 ; Echo it back to user
-        ADD R1, R0, #0      ; ADD R0 + 0 into R1
+        ADD R1, R0, #0      ; Move value from R0 to R1
         LD R0, SP           ; Load a Space
         OUT                 ; Output a Space
         AND R0, R0, #0      ; Set R0 to zero
-        ADD R2, R0, #7      ; Set R2 to 7 count 7 characters
-        ADD R3, R0, #1      ; Set R3 to 1
-COUNT   AND R4, R1, R3      ; Increment our count
-        BRz NOTONE          ; Zero - Branch, no match
+        ADD R2, R0, #7      ; Set R2 to 7 - count 7 characters
+        ADD R3, R0, #1      ; Set R3 to 1 - R3 is shifting bit mask
+COUNT   AND R4, R1, R3      ; Increment our counter
+        BRz NOTONE          ; Zero - Branch, no one detected
         ADD R0, R0, #1      ; Increment counter
-NOTONE  ADD R3, R3, R3      ; Double R3
+NOTONE  ADD R3, R3, R3      ; Double R3 - i.e. shift bit mask to left
         ADD R2, R2, #-1     ; Decrement R2
-        BRp COUNT           ; Still positive, go count again
-        LD R1, ZERO         ; Load ASCII Base
+        BRp COUNT           ; Still positive, check next bit
+        LD R1, ZERO         ; Load ASCII Base for zero
         ADD R0, R0, R1      ; ADD ASCII Offset to count
         OUT                 ; Output count to console
         LD R0, LF           ; Load Linefeed
