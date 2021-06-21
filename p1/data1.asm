9343feeeb6db26f14cc2c6becfd6680c2d2c5b71
mjdargen <mjdargen@gmail.com>
2021-06-21 14:30:53 -0400

    uploaded p1 test cases

---
+++ p1/data1.asm
@@ -0,0 +1,18 @@
+;
+; data1.asm - input test data for p1
+; Test case 1 - short test case with only positive offsets
+;
+
+.ORIG x3200
+
+.FILL x0148
+.FILL x0265
+.FILL x814A
+.FILL x036C
+.FILL xAD36
+.FILL x043D
+.FILL x016C
+.FILL x016F
+.FILL x0003
+
+.END
---
+++ p1/data4.asm
@@ -0,0 +1,17 @@
+;
+; data4.asm - input test data for p1
+; Test case 4 - short test case with positive & negative offsets
+;
+
+.ORIG x3200
+
+.FILL x0348
+.FILL x3348
+.FILL x036C
+.FILL x0165
+.FILL x0E6C
+.FILL x026F
+.FILL x02F6
+.FILL x0003
+
+.END
